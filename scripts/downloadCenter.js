6e060a021fbd4f1592034301a1191c4a93c099fc
emma reed <emmakatharynreed@icloud.com>
2026-03-27 17:06:04 -0600

    Bump version numbers on script tags

--- scripts/downloadCenter.js
+++ scripts/downloadCenter.js
@@ -14,7 +14,7 @@
 // use js to render the html
 
 // Updated Meeting Data
-import { meetings } from "./meetingsData.js";
+import { meetings } from "./meetingsData.js?v=1.1";
 
 // Function to dynamically generate URLs
 function generateDocumentURL(monthId, committee, type) {
