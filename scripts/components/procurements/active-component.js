7db64a837b8785f580e37a2eb3a2587ae4f005d7
emma reed <emmakatharynreed@icloud.com>
2026-04-06 16:10:04 -0600

    Update RFP entries in active-component.js to reflect the correct version 342-2026-03 for Property Liability Workers' Compensation Claims Auditing Services, including updated links and descriptions. Jesus fucking Christ.

--- scripts/components/procurements/active-component.js
+++ scripts/components/procurements/active-component.js
@@ -19,16 +19,16 @@ class ActiveProcurements extends HTMLElement {
           <tbody>
           <tr>
           <td>05/04/2026</td>
-          <td>RFP# 342-2026-02</td>
+          <td>RFP# 342-2026-03</td>
           <td>
-            <a href="/docs/P_and_L_WC_Claims_Auditing_Services_RFP_Amendment_1.pdf" target="_blank">RFP# 342-2026-02 RFP: Workplace Ergonomic Training and Consulting Services - Amendment #1</a>
+            <a href="/docs/P_and_L_WC_Claims_Auditing_Services_RFP_Amendment_1.pdf" target="_blank">RFP# 342-2026-03 RFP Property Liability Workers' Compensation Claims Auditing Services - Amendment #1</a>
           </td>
         </tr>
           <tr>
           <td>04/30/2026</td>
-          <td>RFP# 342-2026-02</td>
+          <td>RFP# 342-2026-03</td>
           <td>
-            <a href="/docs/Property_Liability_Workers_Compensation_Claims_Auditing_Services_RFP_FINAL.pdf" target="_blank">RFP# 342-2026-02 RFP: Workplace Ergonomic Training and Consulting Services</a>
+            <a href="/docs/Property_Liability_Workers_Compensation_Claims_Auditing_Services_RFP_FINAL.pdf" target="_blank">RFP# 342-2026-03 RFP Property Liability Workers' Compensation Claims Auditing Services</a>
           </td>
         </tr>
           <tr>
