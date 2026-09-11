eb6d969f23d072991f573293ab3ccb1f89f0821d
emma reed <emmakatharynreed@icloud.com>
2026-09-11 15:54:44 -0600

    Update active-component.js to include new procurement entries and bump script version to v2.2 for cache control.

--- scripts/components/procurements/active-component.js
+++ scripts/components/procurements/active-component.js
@@ -18,6 +18,20 @@ class ActiveProcurements extends HTMLElement {
           </thead>
           <tbody>
           <tr>
+            <td>09/25/2026</td>
+            <td>RFP# 342-2027-01</td>
+            <td>
+              <a href="/docs/Insurance_Broker_Consulting_Administrative_Services_and_Loss_Prevention_Services_A2.pdf" target="_blank">Amendment #2 - Insurance Broker, Consulting, Administration Services and Loss Prevention Services</a>
+            </td>
+          </tr>
+          <tr>
+            <td>&mdash;</td>
+            <td>RFP# 342-2027-01</td>
+            <td>
+              <a href="/docs/Questions_and_Answers_RFP_342-2027-01.pdf" target="_blank">RFP# 342-2027-01 Insurance Broker Consulting, Administrative Services and Loss Prevention Services - Questions and Answers</a>
+            </td>
+          </tr>
+          <tr>
           <td>09/25/2026</td>
           <td>RFP# 342-2027-01</td>
           <td>
